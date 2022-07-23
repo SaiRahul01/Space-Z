ff582180781af214699d813bb9a5dcde194f267d
Sai Rahul <sairahul0411@gmail.com>
2022-07-24 00:15:27 +0530

    .

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,22 @@
-import { View, Text, StyleSheet,TextInput, TouchableOpacity, ToastAndroid,AsyncStorage } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet,TextInput, TouchableOpacity, ToastAndroid,AsyncStorage, Alert } from 'react-native'
+import React, { useState } from 'react'
 import FA from 'react-native-vector-icons/FontAwesome'
 import II from 'react-native-vector-icons/Ionicons'
 
 const Login = (props) => {
+
+  const [passkey, setpasskey] = useState('')
     const loginuser=()=>{
+      if(passkey==='spacez')
+      {
         ToastAndroid.show("Logged into Space !",2000)
         props.setisloggedin(true)
         AsyncStorage.setItem("isloggedin",true)
+        return;
+      }
+      Alert.alert("Error!","Please Enter a valid Key !")
+
+        
     }
 
 
@@ -17,12 +26,13 @@ const Login = (props) => {
     <View style={ss.root}>
       <View style={{borderWidth:0,borderColor:'white',marginTop:150,margin:28}}>
       <Text style={{color:'white',justifyContent:'center', alignItems:'center',fontSize:28}}>Welcome to Space Z</Text>
+      <Text style={{color:'white'}}>{passkey}</Text>
       </View>
 
       <View style={{marginLeft:30,marginRight:30,marginTop:50,padding:10,flexDirection:'row',justifyContent:'space-around',alignItems:'center'}}>
        
         <View style={{flexGrow:3,marginLeft:30}}>
-        <TextInput placeholder='Enter Key' placeholderTextColor={'white'} style={{borderWidth:0,borderColor:'white',padding:15,backgroundColor:'#5F939A',height:50,color:'white'}} selectionColor='white' secureTextEntry={true}/>
+        <TextInput placeholder='Enter Key' placeholderTextColor={'white'} style={{borderWidth:0,borderColor:'white',padding:15,backgroundColor:'#5F939A',height:50,color:'white'}} value={passkey} onChangeText={setpasskey}  selectionColor='white' secureTextEntry={true}/>
         </View>
         
       </View>
